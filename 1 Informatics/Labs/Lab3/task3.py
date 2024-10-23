a6f59bdc9c3c3d621b46bcba5f05a6954fc62d40
Владислав Дядев <vladislavdyadev@MacBook-Air-Vladislav-2.local>
2024-10-24 02:39:43 +0300

    Added Lab3 Inf

---
+++ 1 Informatics/Labs/Lab3/task3.py
@@ -0,0 +1,45 @@
+"""
+Написать регулярное выражение, которое проверяет корректность email и в качестве
+ответа выдаёт почтовый сервер (почтовый сервер - часть email идущая после "@").
+
+Для простоты будем считать, что почтовый адрес может содержать в себе буквы,
+цифры, "." и "_", а почтовый сервер только буквы и ".". При этом почтовый сервер
+обязательно должен содержать верхний уровень домена (".ru", ".com", etc.).
+"""
+
+import re
+
+
+def task3(text):
+    pattern = r"[a-zA-z0-9\._]+@([a-zA-z]+\.[a-zA-z]+)"
+
+    res = re.fullmatch(pattern, text)
+    if res:
+        ans = res.group(1)
+    else:
+        ans = "Fail!"
+
+    return ans
+
+
+tests = {
+    "alvasid@gmail.com": "gmail.com",
+    "example.ex_123@domen.abcd": "domen.abcd",
+    "ex_ex@domen": "Fail!",
+    "123321@123.ru": "Fail!",
+    "spam.123.today@mail.ru": "mail.ru",
+}
+
+i = 1
+for test in tests:
+    print(f"Тест {i}: {test}")
+    ans = tests[test]
+    res = task3(test)
+    if ans == res:
+        print(f"Тест пройден успешно! Ответ: {res}.")
+    else:
+        print(
+            f"Тест не пройден! Результат программы: {res}. Результат ручного подсчёта: {ans}."
+        )
+    i += 1
+    print("-" * 40)
