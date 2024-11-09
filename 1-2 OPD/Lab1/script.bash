68c73279b2fb848bf2612cba6e1fa671c3751f33
Владислав Дядев <vladislavdyadev@MacBook-Air-Vladislav-4.local>
2024-11-09 15:43:21 +0300

    Bugfix Lab1 OPD

--- 1-2 OPD/Lab1/script.bash
+++ 1-2 OPD/Lab1/script.bash
@@ -73,19 +73,19 @@ echo "====Finished 3===="
 echo "====Started 4===="
 # 4.1
 echo "====4.1===="
-wc -m $(ls -dp ./**/d*) 2>>/tmp/err | sort -nr
+wc -m $(ls -dp */d*) 2>>/tmp/err | sort -nr
 # 4.2
 echo "====4.2===="
 ls -lit riolu5 2>>/tmp/err
 # 4.3
 echo "====4.3===="
-cat $(ls -dp ./**/d*) | sort
+cat $(ls -dp */d*) | sort
 # 4.4
 echo "====4.4===="
 ls -liR riolu5 2>/dev/null | sort -nk3
 # 4.5
 echo "====4.5===="
-ls -lit ./**/*e  2>>/tmp/err | tail -4
+ls -lit *e */*e */*/*e 2>>/tmp/err | tail -4
 # 4.6
 echo "====4.6===="
 cat $(ls -dp *4) 2>&1 | sort
